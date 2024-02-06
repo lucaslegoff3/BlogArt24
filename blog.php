0823b9a67a092132bbb2240f9080c9792b4d866b
ad <chamalo.prv@gmail.com>
2024-02-06 15:06:50 +0100

    Update blog.php

--- blog.php
+++ blog.php
@@ -15,10 +15,16 @@ sql_connect();
         <img src="img_article_1.webp">
         <h1>Festival Accessible aux sourdet malentendant à Bordeaux</h1>
     </div>
+    <div>
+        <img src="img_article_1.webp">
+        <h2>Bordeaux à travers – La musique accessible</h2>
+        <h3>Les Festivals sont des événements devenus incontournables au fil des années. Très à la mode dans les années 70, ils ont su s’adapter au fil des années pour plaire à tout public en proposant de plus en plus de variations de styles musicaux. Cependant, quand on parle d'accessibilité à ces événements, la question des personnes atteintes de surdité se pose. Comment faire en sorte pour qu'elles aussi puissent ressentir la musique ?</h3>
+        <a class="nav-link active" href="/">en savoir plus</a>
+    </div>
+    
+
     
     
 </body>
-
-
 
 <?php require_once 'footer.php'; ?>
