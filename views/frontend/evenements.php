e4a05a4695f4981306a6f03f28973e914d210454
Lucaspasticier <159118986+Lucaspasticier@users.noreply.github.com>
2024-02-06 16:14:24 +0100

    evenements

---
+++ views/frontend/evenements.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evenements</title>
+</head>
+
+<body>
+    <div class="haut">
+        <H1>Evenements</H1>
+        <img src="/xampp/htdocs/blogart24/src/images/Edgar">
+    </div>
+    <div class="img1">
+        <H3>A venir...</H3>
+        <br>
+        <br>
+        <img src="img1">
+        <br>
+        <br>
+        En savoir 
+        <br>
+        plus
+    </div>
+    <div class="img2">
+        <img src="img1">
+        <br>
+        <br>
+        En savoir 
+        <br>
+        plus
+    </div>
+</body>
+</html>
