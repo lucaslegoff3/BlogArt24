2a46381a661ef9bc361b8490f75ec07f76f74d9c
Lucaspasticier <159118986+Lucaspasticier@users.noreply.github.com>
2024-02-06 14:28:01 +0100

    html

--- views/frontend/cgu.php
+++ views/frontend/cgu.php
@@ -14,27 +14,40 @@ Date d'entrée en vigueur : 2 février 2024
 <br>
 En utilisant ce blog, vous acceptez pleinement et sans réserve les présentes Conditions Générales d'Utilisation (CGU). Si vous n'êtes pas d'accord avec l'une de ces conditions, veuillez ne pas utiliser ce blog.
 <br>
-
+<br>
 1. Acceptation des conditions
+<br>
 En accédant et en utilisant ce blog, vous reconnaissez avoir lu, compris et accepté les présentes CGU.
 <br>
+<br>
 2. Description des services
+<br>
 Ce blog, exploité par Lucas Le Goff, propose des articles, des informations, et d'autres contenus liés à l’Accessibilité de la musique au personnes sourde et malentendante. 
 <br>
+<br>
 3. Responsabilités
+<br>
 Le propriétaire du blog s'efforce de fournir des informations exactes et à jour, mais ne garantit pas l'exactitude, l'exhaustivité ou la pertinence de son contenu. L'utilisation des informations et du contenu du blog est à vos propres risques.
 <br>
+<br>
 4. Propriété intellectuelle
+<br>
 Tous les contenus, textes, images, graphiques, logos, vidéos et autres éléments présents sur ce blog sont protégés par les lois sur les droits d'auteur et sont la propriété exclusive de Lucas Le Goff ou de ses concédants de licence. Toute utilisation non autorisée de ces contenus est strictement interdite.
 L'entreprise est située au 1 Rue Jacques Ellul, 33800 Bordeaux.
 <br>
+<br>
 5. Données personnelles
+<br>
 Nous collectons, utilisons et traitons vos données personnelles conformément à notre Politique de Confidentialité. En utilisant ce blog, vous consentez à la collecte et à l'utilisation de vos données personnelles comme décrit dans ladite Politique de Confidentialité.
 <br>
+<br>
 6. Litiges
+<br>
 Tout litige découlant de l'utilisation de ce blog sera régi par les lois en vigueur en France. Tout litige sera soumis à la juridiction exclusive des tribunaux de Bordeaux.
 <br>
+<br>
 7. Modifications des CGU
+<br>
 Nous nous réservons le droit de modifier ces CGU à tout moment. Les modifications seront publiées sur cette page avec la date d'entrée en vigueur mise à jour. Il vous incombe de consulter régulièrement cette page pour rester informé des éventuelles modifications.
 <br>
 Si vous avez des questions ou des préoccupations concernant ces CGU, veuillez nous contacter à l'adresse e-mail suivante : lucas.le-goff@mmibordeaux.com
@@ -46,6 +59,7 @@ En l'absence d'un capital social financier formel, chaque membre de l'équipe co
 Nous croyons en l'importance de favoriser un environnement inclusif et collaboratif, où la valeur de chaque contribution individuelle s'exprime dans le succès collectif de notre entreprise.
 <br>
 Cependant, si vous souhaitez intégrer une mention sur le capital social dans vos CGU, voici une suggestion :
+<br>
 <br>
 Capital Social
 <br>
