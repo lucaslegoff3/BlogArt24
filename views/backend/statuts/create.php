a7bce9385925813396968ecd42ae09668049a6e1
edgar2645 <edgar.quemere@mmibordeaux.com>
2024-02-06 10:17:22 +0100

    initial push

---
+++ views/backend/statuts/create.php
@@ -0,0 +1,25 @@
+<?php
+include '../../../header.php';
+?>
+
+<!-- Bootstrap form to create a new statut -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Création nouveau Statut</h1>
+        </div>
+        <div class="col-md-12">
+            <!-- Form to create a new statut -->
+            <form action="<?php echo ROOT_URL . '/api/statuts/create.php' ?>" method="post">
+                <div class="form-group">
+                    <label for="libStat">Nom du statut</label>
+                    <input id="libStat" name="libStat" class="form-control" type="text" autofocus="autofocus" />
+                </div>
+                <br />
+                <div class="form-group mt-2">
+                    <button type="submit" class="btn btn-primary">Confirmer create ?</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
