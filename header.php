d4adcd66b017eb3822311c798f57748d86fe7a35
edgar2645 <edgar.quemere@mmibordeaux.com>
2024-02-06 14:40:33 +0100

    Header

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="src/css/style.css" />
     <!-- Bootstrap CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-    <link rel="shortcut icon" type="image/x-icon" href="src/images/article.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="./src/images/LS_logo_noir.webp" />
 </head>
 <?php
 //load config
@@ -17,14 +17,23 @@ require_once 'config.php';
 <body>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Blog'Art 24</a>
+    <img src="./src/images/LS_logo_noir.webp" alt="Logo LSART">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Acceuil</a>
+        </li>
+        <li>
+        <a class="nav-link active" href="/">Evenements</a>
+        </li>
+        <li>
+        <a class="nav-link active" href="/">Blog</a>
+        </li>
+        <li>
+        <a class="nav-link active" href="/">À propos</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/views/backend/dashboard.php">Admin</a>
@@ -34,10 +43,14 @@ require_once 'config.php';
     <!--right align-->
     <div class="d-flex">
       <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Rechercher sur le site…" aria-label="Search" >
+        <div class="input-group mb-3">
+        <span class="input-group-text">
+        <img src="./src/images/search.png" alt="Pictogramme loupe">
+        </span>
+        <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search">
+      </div>
       </form>
-      <a class="btn btn-primary m-1" href="/views/backend/security/login.php" role="button">Login</a>
-      <a class="btn btn-dark m-1" href="/views/backend/security/signup.php" role="button">Sign up</a>
+      <a class="btn btn-primary m-1" href="/views/backend/security/login.php" role="button">Connexion</a>
     </div>
   </div>
 </nav>
