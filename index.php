f8d0e8b086e282f9820a75a8891d8995d4a59a8f
Barth0 <barthoduteil@gmail.com>
2024-02-06 14:30:03 +0100

    index et footer

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
 <?php 
 require_once 'header.php';
 sql_connect();
-
-
 ?>
 
-
+<head>
+    <h1>LA MUSIQUE ACCESSIBLE A TOUS</h1>
+    <button>Découvrir</button>
+</head>
+<hr>
+<section>
+<div>
+        <img src="img_art_1.webp">
+        <h3>Article</h3>
+        <h6>en savoir plus</h6>
+    </div>
+    <div>
+        <img src="img_art_1.webp">
+        <h3>Article</h3>
+        <h6>en savoir plus</h6>
+    </div>
+    <div>
+        <img src="img_art_1.webp">
+        <h3>Article</h3>
+        <h6>en savoir plus</h6>
+    </div>
+    <div>
+        <img src="img_art_1.webp">
+        <h3>Article</h3>
+        <h6>en savoir plus</h6>
+    </div>
+</section>
+<hr>
+<section>
+    <h2>Qui sommes nous ?</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vel nulla et ducimus blanditiis aut sint 
+        numquam ullam perspiciatis, culpa exercitationem enim unde voluptas, totam repellat laborum obcaecati. 
+        Quasi, ullam.</p>
+    <h6>en savoir plus</h6>
+</section>
+<hr>
 
 
 <?php require_once 'footer.php'; ?>
