3d21e994dad5b20964e7c1866ef7f00ae0f9e0d4
Barth0 <barthoduteil@gmail.com>
2024-02-07 17:50:59 +0100

    blog + header inscription

--- views/frontend/blog.php
+++ views/frontend/blog.php
@@ -3,44 +3,42 @@ require_once '../../header.php';
 sql_connect();
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>blog</title>
-</head>
-
 <body>
-    <div>
-        <img src="img_blog.webp">
-        <h1>Festival Accessible aux sourdet malentendant à Bordeaux</h1>
-    </div>
-    <div>
+<div class="header">
+    <h1 class="accueil">Festival Accessible aux sourds et malentendants à Bordeaux</h1>
+</div>
+<section class=blog>
+    <div class="blog_sections">
+        <div>
+            <h2>Bordeaux à travers – La musique accessible</h2>
+            <h3>Les Festivals sont des événements devenus incontournables au fil des années. Très à la mode dans les années 70, ils ont su s’adapter au fil des années pour plaire à tout public en proposant de plus en plus de variations de styles musicaux. Cependant, quand on parle d'accessibilité à ces événements, la question des personnes atteintes de surdité se pose. Comment faire en sorte pour qu'elles aussi puissent ressentir la musique ?</h3>
+            <a class="nav-link active" href="/">en savoir plus</a>
         <img src="img_article_1.webp">
+        </div>
+        </div>
+    <div class="blog_sections">
+        <div>
         <h2>Bordeaux à travers – La musique accessible</h2>
         <h3>Les Festivals sont des événements devenus incontournables au fil des années. Très à la mode dans les années 70, ils ont su s’adapter au fil des années pour plaire à tout public en proposant de plus en plus de variations de styles musicaux. Cependant, quand on parle d'accessibilité à ces événements, la question des personnes atteintes de surdité se pose. Comment faire en sorte pour qu'elles aussi puissent ressentir la musique ?</h3>
         <a class="nav-link active" href="/">en savoir plus</a>
-    </div>
-    <div>
         <img src="img_article_2.webp">
+        </div>
+    </div>
+    <div class="blog_sections">
+        <div>
         <h2>Bordeaux à travers – La musique accessible</h2>
         <h3>Les Festivals sont des événements devenus incontournables au fil des années. Très à la mode dans les années 70, ils ont su s’adapter au fil des années pour plaire à tout public en proposant de plus en plus de variations de styles musicaux. Cependant, quand on parle d'accessibilité à ces événements, la question des personnes atteintes de surdité se pose. Comment faire en sorte pour qu'elles aussi puissent ressentir la musique ?</h3>
         <a class="nav-link active" href="/">en savoir plus</a>
-    </div>
-    <div>
         <img src="img_article_3.webp">
-        <h2>Bordeaux à travers – La musique accessible</h2>
-        <h3>Les Festivals sont des événements devenus incontournables au fil des années. Très à la mode dans les années 70, ils ont su s’adapter au fil des années pour plaire à tout public en proposant de plus en plus de variations de styles musicaux. Cependant, quand on parle d'accessibilité à ces événements, la question des personnes atteintes de surdité se pose. Comment faire en sorte pour qu'elles aussi puissent ressentir la musique ?</h3>
-        <a class="nav-link active" href="/">en savoir plus</a>
+        </div>
     </div>
-    <div>
-        <img src="img_article_4.webp">
+    <div class="blog_sections">
+        <div>
         <h2>Bordeaux à travers – La musique accessible</h2>
         <h3>Les Festivals sont des événements devenus incontournables au fil des années. Très à la mode dans les années 70, ils ont su s’adapter au fil des années pour plaire à tout public en proposant de plus en plus de variations de styles musicaux. Cependant, quand on parle d'accessibilité à ces événements, la question des personnes atteintes de surdité se pose. Comment faire en sorte pour qu'elles aussi puissent ressentir la musique ?</h3>
         <a class="nav-link active" href="/">en savoir plus</a>
+        <img src="img_article_4.webp">
+        </div>
     </div>
     <div class="commentaires">
         <h2>Commentaires</h2>
@@ -53,7 +51,7 @@ sql_connect();
         </form>
     </div>
 
-
+</section>
 </body>
 
 <?php require_once '../../footer.php'; ?>
