076baadc73d1facf1122633ce6f3bb76880b49c7
Barth0 <barthoduteil@gmail.com>
2024-02-07 17:07:37 +0100

    cgu css

--- views/frontend/cgu.php
+++ views/frontend/cgu.php
@@ -1,7 +1,63 @@
-<?php 
-require_once '../../header.php';
-sql_connect();
+<?php
+require_once '../../config.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>LSART</title>
+  <link href="../../src/css/style.css" rel="stylesheet" type="text/css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+  <link rel="shortcut icon" type="image/x-icon" href="<?php echo ROOT_URL; ?>/src/images/Logo_LSART_Google.webp" />
+</head>
+
+
+<body>
+<nav id="navbar" class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <img src="../../src/images/LS_logo_noir.webp" alt="Logo LSART" id="logo_header" class="logo_header">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Accueil</a>
+        </li>
+        <li>
+        <a class="nav-link active" href="./evenements.php">Evenements</a>
+        </li>
+        <li>
+        <a class="nav-link active" href="/">Blog</a>
+        </li>
+        <li>
+        <a class="nav-link active" href="./equipe.php">À propos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/views/backend/dashboard.php">Admin</a>
+        </li>
+      </ul>
+    </div>
+    <!--right align-->
+    <div class="d-flex">
+      <form id="search_bar" class="d-flex" role="search">
+        <div class="input-group mb-3 search_bar">
+        <span class="input-group-text search_icon">
+        <img src="../../src/images/search.png" alt="Pictogramme loupe" id="picto_loupe">
+        </span>
+        <input class="form-control me-2 search_input" type="search" placeholder="Rechercher" aria-label="Search">
+      </div>
+      </form>
+      <a id="connexion" class="btn btn-primary m-1" href="/views/backend/security/login.php" role="button">Connexion</a>
+      <a id="inscription" class="btn btn-primary m-1" href="/views/backend/security/login.php" role="button">Inscription</a>
+    </div>
+  </div>
+</nav>
+
+<section class="cgu">
 
     <h1>Mentions légales</h1>
     <br>
@@ -68,7 +124,33 @@ sql_connect();
     <br>
     Nous croyons en l'importance de favoriser un environnement inclusif et collaboratif, où la valeur de chaque contribution individuelle s'exprime dans le succès collectif de notre entreprise.
 
-
+</section>
 </body>
+<footer>
+  <div>
+    <img src="../../src/images/LS_logo_blanc.webp">
+    <p>LS'Art © 2024</p>
+  </div>
+  <div>
+    <h5>Plan du site</h5>
+    <p>Accueil</p>
+    <p>Évènements</p>
+    <p>Blog</p>
+    <p>À propos</p>
+    <p>Recherche</p>
+    <p>Connexion</p>
+  </div>
+  <div>
+    <h5>Légal</h5>
+    <p>Gestion des cookies</p>
+    <p>Mention légales</p>
+    <p>Gestion de vos données</p>
+  </div>
+  <div>
+    <h5>Nous suivre</h5>
+    <p>S'abonner à la Newsletter</p>
+    <button>Nous contacter</button>
+  </div>
 
+</footer>
 </html>
