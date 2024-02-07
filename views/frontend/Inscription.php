7827d5a187335e7f86e2eb7f75e887524c837a24
Lucaspasticier <159118986+Lucaspasticier@users.noreply.github.com>
2024-02-07 10:35:35 +0100

    inscription

--- views/frontend/Inscription.php
+++ views/frontend/Inscription.php
@@ -6,30 +6,34 @@
     <title>Inscription</title>
 </head>
 <body>
+    <H1>Inscrivez-vous</H1>
+    <!-------------------------------- Ligne à rjt CSS ------------------------------------------->
 
-    <H1>Connectez-vous</H1>
+    <div class="formulaire">
+        <div class="mail">
+            <H2>E-Mail</H2>
+            <!-------------------------------- rjt rect mail + txt ------------------------------------------->
+            Email
+        </div>   
 
-<!-------------------------------- Ligne à rjt CSS ------------------------------------------->
+        <div class="mdp">
+            <H2>Mot de passe</H2>
+            <!-------------------------------- rjt rect mdp + txt ------------------------------------------->
+            Mot de passe (6 caractères minimum)
 
-    <div class="Nom_prenom_mail">
-        <!-------------------------------- rajt rectangles nom + prenom + mail ------------------------------------------->
-        <!-------------------------------- Mettre mots dans rectangles ------------------------------------------->
-        Nom
-        Prénom
-        Email
+        <div class="confirmer mdp">
+            <H2>Confirmer mot de passe</H2>
+            <!-------------------------------- rjt rect confirmer mdp + txt ------------------------------------------->
+            Confirmation
+        </div>  
+
+        <div class="afficher mdp">
+            <!-------------------------------- rjt carré afficher mdp + txt à gauche ------------------------------------------->
+            Afficher le mot de passe
+        </div>        
     </div>
 
-    <div class="Message">
-        <H2>Message</H2>
-        <!-------------------------------- rajt rectangle "Objet du msg" + txt ------------------------------------------->
-        Objet du Message
-
-        <!-------------------------------- rajt rectangle "Votre message" + txt  ------------------------------------------->
-        Votre message...
-    </div>
-
-    <!-------------------------------- rajt rectangle "formulaire" + txt ------------------------------------------->
-        Envoyer formulaire
-      
+        <!-------------------------------- rjt bouton inscrire + txt + ombre ------------------------------------------->
+        En cliquant sur « S'inscrire », vous confirmez que vous acceptez les Conditions générales d'utilisation et notre Politique de confidentialité qui vous informe des modalités de traitement de vos données personnelles ainsi que de vos droits sur ces données.
 </body>
 </html>
