e10d47433885a3686c4331f9efe74b44f01a503e
lucaslegoff3 <lucas.le-goff@mmibordeaux.com>
2024-02-08 10:27:09 +0100

    remove unused ?

--- api/articles/delete.php
+++ api/articles/delete.php
@@ -4,6 +4,6 @@ require_once '../../functions/ctrlSaisies.php';
 
 $numArt = ctrlSaisies($_POST['numArt']);
 
-sql_delete('ARTICLES', "numArt = $numArt");
+sql_delete('ARTICLE', "numArt = $numArt");
 
 header('Location: ../../views/backend/articles/list.php');
