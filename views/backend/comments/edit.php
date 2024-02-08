87914655fcac78798fa40e36630d638dd664f5e0
lucaslegoff3 <lucas.le-goff@mmibordeaux.com>
2024-02-08 12:33:16 +0100

    edit

--- views/backend/comments/edit.php
+++ views/backend/comments/edit.php
@@ -1,39 +1,30 @@
 <?php
 include '../../../header.php';
 
-if (isset($_GET['idCommentaire'])) {
-    $idCommentaire = $_GET['idCommentaire'];
-
-    $result = sql_select("COMMENTAIRE", "numMemb, numArt, libCom", "idCommentaire = $idCommentaire");
+if (isset($_GET['numCom'])) {
+    $numCom = $_GET['numCom'];
+    $result = sql_select("COMMENT", "libCom", "numCom = $numCom");
     if (!empty($result)) {
-        $numMemb = $result[0]['numMemb'];
-        $numArt = $result[0]['numArt'];
         $libCom = $result[0]['libCom'];
     } else {
     }
 }
 ?>
 
+<!-- Bootstrap form to create a new statut -->
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Modification d'un commentaire</h1>
+            <h1>Modification Commentaire</h1>
         </div>
         <div class="col-md-12">
             <form action="<?php echo ROOT_URL . '/api/comments/update.php' ?>" method="post">
                 <div class="form-group">
-                    <label for="numMemb">ID du membre</label>
-                    <input id="numMemb" name="numMemb" class="form-control" type="text" value="<?php echo isset($numMemb) ? htmlspecialchars($numMemb) : ''; ?>" readonly="readonly" />
+                    <label for="libCom">Nom du statut</label>
+                    <input id="numCom" name="numCom" class="form-control" style="display: none" type="text" value="<?php echo isset($numCom) ? htmlspecialchars($numCom) : ''; ?>" readonly="readonly" />
+                    <input id="libCom" name="libCom" class="form-control" type="text" value="<?php echo htmlspecialchars($libCom); ?>" />
+                    <input id="attModOK" name="attModOK" class="form-control" type="text" value="<?php echo htmlspecialchars($commentaire['attModOK']); ?>" />
                 </div>
-                <div class="form-group">
-                    <label for="numArt">ID de l'article</label>
-                    <input id="numArt" name="numArt" class="form-control" type="text" value="<?php echo isset($numArt) ? htmlspecialchars($numArt) : ''; ?>" readonly="readonly" />
-                </div>
-                <div class="form-group">
-                    <label for="libCom">Commentaire</label>
-                    <textarea id="libCom" name="libCom" class="form-control"><?php echo isset($libCom) ? htmlspecialchars($libCom) : ''; ?></textarea>
-                </div>
-                <input type="hidden" name="idCommentaire" value="<?php echo isset($idCommentaire) ? htmlspecialchars($idCommentaire) : ''; ?>" />
                 <br />
                 <div class="form-group mt-2">
                     <button type="submit" class="btn btn-primary">Enregistrer les modifications</button>
@@ -42,7 +33,3 @@ if (isset($_GET['idCommentaire'])) {
         </div>
     </div>
 </div>
-
-<?php
-include '../../../footer.php';
-?>
