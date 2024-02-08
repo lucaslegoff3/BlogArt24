6a3a925010f766b3e337bab24565f07416463fc2
ad <chamalo.prv@gmail.com>
2024-02-08 10:05:39 +0100

    Update contact.php

--- views/frontend/contact.php
+++ views/frontend/contact.php
@@ -4,9 +4,12 @@
     <div class="formulaire">
         <H1 class="inscrivezvous">Contactez-nous !</H1>
         <hr>
-        <div class="Nom_prenom_mail">
+        <div class="Nom">
             <input type="text" id="name" name="name" required minlength="0" maxlength="20" size="40" placeholder="Nom"/>
+        </div>
+        <div class="Prenom">
             <input type="text" id="name" name="name" required minlength="0" maxlength="20" size="40" placeholder="Prénom"/>
+        <div class="insertemail">
             <input type="text" id="name" name="name" required minlength="0" maxlength="20" size="40" placeholder="Email"/>
         </div>
 
